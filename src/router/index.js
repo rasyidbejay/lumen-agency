a2aaf16aedb19b59706a4af805551af93759263b
Rashid <rasyidbejay@gmail.com>
2026-03-13 06:06:17 +0800

    fix: use hash router for GitHub Pages compatibility

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import HomeView         from '../views/HomeView.vue'
 import ServicesView     from '../views/ServicesView.vue'
@@ -17,7 +17,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
   scrollBehavior() {
     return { top: 0, behavior: 'smooth' }
